3d48d58a486ad5a7b131161bdc314e1640bac770
Taegus <taegus.cromis@gmail.com>
2019-03-30 21:37:07 +0100

    added lint added uni-beautify cleaned code

--- index.js
+++ index.js
@@ -1,52 +1,52 @@
 // redisDemo.js
 const bodyParser = require("body-parser");
-const NodeCache = require( "node-cache" );
+const NodeCache = require("node-cache");
 const express = require("express");
-const config = require('./config.json');
-const cors = require('cors');
-const path = require('path');
-const fs = require('fs');
+const config = require("./config.json");
+const cors = require("cors");
 
-var nodeCache = new NodeCache( { stdTTL: config.cache.expire, checkperiod: config.cache.checkPeriod } ); // the cache object
+var nodeCache = new NodeCache({stdTTL: config.cache.expire, checkperiod: config.cache.checkPeriod}); // the cache object
 var app = express(); // create express app
 // use the json parser for body
 app.use(bodyParser.json());
 app.use(cors());
 
 // start listener
-app.listen(config.server.port, () => { 
-    console.log("Server running on port " + config.server.port); 
+app.listen(config.server.port, () => {
+  console.log("Server running on port " + config.server.port);
 });
 
 // get request for the list of all active nodes
-app.get("/pool/list", (req, res, next) => {
-    nodeCache.keys( function( err, keys ) {
-        if( !err ) {
-            var nodeList = [];
+app.get("/pool/list", (req, res) => {
+  nodeCache.keys(function (err, keys) {
+    if (!err) {
+      var nodeList = [];
 
-            keys.forEach(function(value) {
-                nodeList.push(nodeCache.get(value));
-            }); 
+      keys.forEach(function (value) {
+        nodeList.push(nodeCache.get(value));
+      });
 
-            // return the list
-            res.json({ "success": true, "list": nodeList });
-        } else {
-            res.json({ "success": false, "list": [] });
-        }
-    });
+      // return the list
+      res.json({success: true, list: nodeList});
+    } else {
+      res.json({success: false, list: []});
+    }
+  });
 });
 
 // post request for updating the node data
-app.post("/pool/update", (req, res, next) => {
-    if (req.body) {
-        req.body.ip = req.header('x-forwarded-for') || req.connection.remoteAddress;
-        res.json({ "success": nodeCache.set(req.body.id, req.body, 600) });    
-    }
+app.post("/pool/update", (req, res) => {
+  if (req.body) {
+    req.body.ip = req.header("x-forwarded-for") || req.connection.remoteAddress;
+    res.json({
+      success: nodeCache.set(req.body.id, req.body, 600)
+    });
+  }
 });
 
 // handle any application errors
 app.use(function (err, req, res, next) {
-    if (err) {
-        next(err);
-    }
+  if (err) {
+    next(err);
+  }
 });
