e79391e9e66d8a331799985668aeed35347b8f68
Taegus <taegus.cromis@gmail.com>
2019-04-08 15:04:21 +0200

    check the node if it has open API port and if it returns a response on first contact added random API for selection a random node from the pool

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 // redisDemo.js
 const bodyParser = require("body-parser");
 const NodeCache = require("node-cache");
-const jsonPath = require('jsonpath');
+const vsprintf = require("sprintf-js").vsprintf;
 const express = require("express");
 const config = require("./config.json");
 const Ddos = require('ddos');
 const cors = require("cors");
+const CCX = require("conceal-js");
 
 // log the denial requests for pool
 const onDenial = function (req) {
@@ -22,7 +23,7 @@ app.use(cors());
 
 // start listener
 app.listen(config.server.port, () => {
-  console.log("Server running on port " + config.server.port);
+  console.log(vsprintf("Server running on port %d", [config.server.port]));
 });
 
 // get request for the list of all active nodes
@@ -43,12 +44,47 @@ app.get("/pool/list", (req, res) => {
   });
 });
 
+// get the random node back to user
+app.get("/pool/random", (req, res) => {
+  nodeCache.keys(function (err, keys) {
+    if (!err) {
+      var randomKey = keys[Math.floor(Math.random() * keys.length)];
+      var randomNode = nodeCache.get(randomKey);
+
+      if (randomNode) {
+        res.json({ success: true, url: vsprintf("%s:%d", [randomNode.nodeHost, randomNode.nodePort]) });
+      } else {
+        res.json({ success: false });
+      }
+    } else {
+      res.json({ success: false });
+    }
+  });
+});
+
 // post request for updating the node data
 app.post("/pool/update", (req, res) => {
-  if (req.body) {
-    res.json({
-      success: nodeCache.set(req.body.id, req.body, 600)
-    });
+  if ((req.body) && (req.body.id) && (req.body.nodeHost) && (req.body.nodePort)) {
+    // check if node is already in pool
+    if (!nodeCache.get(req.body.id)) {
+      // initialize the conceal API with the client IP and daemon port
+      var CCXApi = new CCX(vsprintf("http://%s", [req.body.nodeHost]), "3333", req.body.nodePort);
+
+      // if first request check if alive
+      CCXApi.info().then(data => {
+        res.json({
+          success: nodeCache.set(req.body.id, req.body, 600)
+        });
+      }).catch(err => {
+        res.json({
+          success: false
+        });
+      });
+    } else {
+      res.json({
+        success: nodeCache.set(req.body.id, req.body, 600)
+      });
+    }
   }
 });
 
